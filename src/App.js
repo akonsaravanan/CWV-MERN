7fe3dbea3d8c1721bc114edf12f17785a2d381ae
saravanan <saravananrajendran1993@gmail.com>
2022-05-19 22:56:04 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUserLogin } from "./redux/features/authSlice";
+import AddEditTour from "./pages/AddEditTour";
+import TourDetail from "./pages/TourDetail";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import TagTours from "./pages/TagTours";
 
 function App() {
+  // To persist the current logged user state
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("user-profile"));
+  const data = window.location.pathname.startsWith("/tour/");
+
+  useEffect(() => {
+    dispatch(setUserLogin(user));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {!data ? <Header /> : <Header style={{ display: "none" }} />}
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tour/q" element={<Home />} />
+          <Route path="/tour/tag/:tag" element={<TagTours />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/addtour"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/addtour/:id"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/tour/:id" element={<TourDetail />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
